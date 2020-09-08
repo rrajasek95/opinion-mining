edd4c2b22dc7ab2088cd04763c5af8febfd47977
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-09-07 20:47:29 -0700

    Add simple regex association patterns

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 import spacy
+from spacy.tokens import Token
+
+import re
 import neuralcoref
 
 from tqdm.auto import tqdm
@@ -78,6 +81,7 @@ class Pipeline():
         super().__init__()
         self.nlp = spacy.load('en_core_web_lg')
         neuralcoref.add_to_pipe(self.nlp)
+        self._configure_tokenizer()
 
         # We treat entities and aspects to be the same
         self.aspect_lexicon = {
@@ -97,6 +101,35 @@ class Pipeline():
 
         self.parser = Parser()
 
+        self.simple_association_pattern = re.compile(
+            "(pizza|lasagna|bruschetta|gelato|gnocchi) (is|was) (\w+)")
+        
+
+    def _configure_tokenizer(self):
+        def is_pronominal(token):
+            return token.text.lower() in ("it", "they")
+        
+        def is_singular_item(token):
+            return token.text.lower() == "it" and token.pos_ == "PRON"
+
+        def is_plural_item(token):
+            return token.text.lower() == "they"
+
+        def is_quantifier(token):
+            return token.text.lower() in ("every")
+        
+        def is_anaphora(token):
+            return token._.is_pronominal or token._.is_quantifier
+
+        def is_resolved_pronoun(token):
+            return token._.in_coref and token.pos_ == "PRON"
+
+        Token.set_extension("is_pronominal", getter=is_pronominal)
+        Token.set_extension("is_resolved_pronoun", getter=is_resolved_pronoun)
+        Token.set_extension("is_quantifier", getter=is_quantifier)
+        Token.set_extension("is_anaphora", getter=is_anaphora)
+        Token.set_extension("is_singular_item", getter=is_singular_item)
+
     def _process_matched_aspect_label(self, token):
         text = token.text.lower()
         return self.plural_aspects.get(text, text)
@@ -107,42 +140,54 @@ class Pipeline():
     def _is_direct_keyword(self, token):
         return token.text.lower() in self.aspect_lexicon
 
-    def _is_anaphora(self, token):
-        return token._.in_coref and token.pos_ == "PRON"
-
     def _parse_review(self, doc):
         dprint(doc._.coref_clusters)
         aspect_opinions = defaultdict(list)
+
+        previous_sentence_mentions = []
+        current_sentence_mentions = []
+
         for token in doc:
+            if token.is_sent_start:
+                previous_sentence_mentions = current_sentence_mentions
+                current_sentence_mentions = []
+            
             dprint(token.text, token.pos_, token.dep_, token.head.text, token.head.pos_, [child.text for child in token.children])
             
             if self._is_direct_keyword(token):
                 parses = self.parser.parse_zhuang_phrases(token)
 
                 aspect_label = self._process_matched_aspect_label(token)
+                current_sentence_mentions.append(aspect_label)
                 if parses:
                     aspect_opinions[aspect_label] += parses
-                
-            elif self._is_anaphora(token):
-                for cluster in token._.coref_clusters:
-                    aspect_count = 0
-                    # For any anaphora, match against the head
-                    for main_token in cluster.main:
+            elif token._.is_anaphora:
+                if token._.is_singular_item:
+                    print(token.text)
                     
-                        matched_aspect = None
-                        if main_token.text.lower() in self.aspect_lexicon:
-                            
-                            aspect_count += 1
-                            matched_aspect = self._process_matched_aspect_label(main_token)
-                            dprint(matched_aspect)
+                    if len(current_sentence_mentions) > 0:
+                        matched_aspect = current_sentence_mentions[-1] 
+                    elif len(previous_sentence_mentions) > 0:
+                        matched_aspect = previous_sentence_mentions[-1]
+                    else:
+                        # This may be a dummy pronoun
+                        continue
+                    dprint(matched_aspect)
+                    parses = self.parser.parse_zhuang_phrases(token)
+                    if parses:
+                        dprint(matched_aspect)
+                        aspect_opinions[matched_aspect] += parses
+        
+        for g in re.finditer(self.simple_association_pattern, doc.text):
+            sentiment = g.group(1, 3)
+            item_opinions = aspect_opinions[sentiment[0]]
+            for opinion in item_opinions:
+                if sentiment[1] in opinion: 
+                    # Check if we have a more complete parse using our earlier rules
+                    break
+            else:
+                item_opinions.append(sentiment[1])
 
-                        
-                    if aspect_count == 1:
-                        parses = self.parser.parse_zhuang_phrases(token)
-
-                        if parses:
-                            dprint(matched_aspect)
-                            aspect_opinions[matched_aspect] += parses
         dprint("\n")
         return aspect_opinions
 
--- test_on_examples.py
+++ test_on_examples.py
@@ -36,29 +36,12 @@ def all_lexicon_based_reviews():
 
 def cherry_picked_examples():
     return [
-        # "We visited Pittsburgh last week for my 31st Birthday and booked Dish Osteria as our dinner spot. The restaurant is very cozy. Seating is limited, so I would suggest a reservation. The dinner menu changes daily, but overall, expect scrumptious seafood, pasta, and salad. First of all, the bread is delicious and fresh. We enjoyed snacking on it before receiving our antipasta  and salad. We ordered the Insalata Mista della Casa; the salad was the perfect blend of peppery and crisp greens. I would highly recommend ordering it. Both of the antipastas we requested were the standouts of our meal. The Carpaccio di Manzo literally melted in your mouth. The beef was incredibly fresh and sliced to perfection, and the Reggiano was a nice touch. The cozze was the best bowl of mussels either of us have ever devoured  (and I make mussels regularly at home). The taste was somehow delicate and full of flavor simultaneously. I can not imagine a better way to start off your dinner than with these two dishes. By the time we ate our main courses, we were already drunk on the flavors Dish Osteria has to offer. I ordered the Spaghetti ai Frutti di Mare. All of the shellfish was fresh and cooked to perfection. The spaghetti was a firm al dente, which is exactly how I prefer it. Amazing dish. My husband ordered the Saltimbocca di Vitello. The meat was delicious, but somehow the least impressive part of the plate. The butter potato gnocchi was seriously Heavenly! The texture was like fluffy marshmallow, and the flavor - buttery goodness. If I could eat it every day, I would. The brussel's sprouts were sauteed well and incredibly flavorful. Overall, our dinner was an incredibly experience, and if we lived in Pittsburgh, Dish is a place we would dine at regularly. We will remember our dining experience at Dish Osteria for years to come. It was simply delicious, and the best restaurant we visited while in Pittsburgh."
-        # "I've had bad experiences at other Pizza Hut shops, but this one always gives me well made pizzas. Pizza is always fresh, never burnt, always done by the time I get there. There's always people in there so I'm assuming they feel the same way. Try the stuffed crust and crust toppers."
-        # "We found this place from the awesome Yelp! Reviews. It did not disappoint!! From my husband's fried mozzarella to my gluten free spinach artichoke dip, then on to the fabulous gluten free pizza, we left stuffed. We were celebrating our honeymoon in the area and through conversation they found out and gave us an appetizer on the house! If we lived here this would be a go to spot for us! Thank you NYPD! You made us feel so special and full!"
-        # 'One of my favorite restaurants in the world. I’ve been there over a dozen times and this time I decided to have the pizza and the lasagna. The pizza was incredible while the lasagna was undercooked. When I went there last, I had the gnocchi and it was mindnumbingly good.',
         """
-        In the heart of Chinatown, I discovered it enroute to Kensington Market one day. It's quite hard to see, if you don't know it's there. First experience was very positive - would definitely return!
+        "Delicious pizza! I had the Lambretta special pizza, minus the prosciutto because I was in the mood for vegetarian (I'm sure it's delicious with the prosciutto too though). It was HUGE but it was thin crust and it was all I was having and I was hungry so I ate the whole thing myself!
 
-ATMOSPHERE: Small space. Think Banh Mi Boys, and other trendy over-the-counter eateries. Vibe is casual. Free WiFi is nice.
+The service was very fast and friendly, and the atmosphere and decor of the restaurant were very pleasant and modern. 
 
-SERVICE: Okay. Staff does their job, but not overly helpful upon entering the space. Maybe because it's an over-the-counter style place, but it would have been nice to be walked through their ordering process and menu.
-
-PRICE: Average. Depends on the size of pizza you order.The small pizza ($7-9.50) is filling for one. Large size pizza ($13-18.50) is good for sharing... or one VERY hungry person.  Pasta portions are very generous for the price tag ($9-14)
-
-
-FOOD: My friends and I were here for the first time, and we thought it was a pizza place so we all ordered pizza. It wasn't until we were waiting for the food to come, that we looked around and realised EVERY other table around us only had pasta on them. Guess we gotta come back to try! We did notice that the menu said their pasta is homemade. So will definitely come back.
-
-Here's the pizza we ordered:
-
-Peking Duck Pizza - Very tasty! Rich flavours that work well. But it's heavy, so come hungry - or order to share.
-
-Margherita - Good, but not the best. I recommend trying the more "special" toppings instead.
-
-Shall return, and/or try other locations.
+I will definitely come back here! The pizza was made from high quality, authentic ingredients, and I didn't feel stuffed in a bad way like I do at other pizza places. I felt great afterwards."
         """
     ]
 
@@ -70,6 +53,6 @@ if __name__ == "__main__":
 
     # pprint(list(enumerate(pipeline.extract_descriptions(get_annotated_examples_all()))))
     # pprint(list(enumerate(pipeline.extract_descriptions(all_lexicon_based_reviews()))))
-    pprint(list(enumerate(pipeline.extract_descriptions(cherry_picked_examples()))))
-    # pprint(list([(i + 1, e) for (i, e) in enumerate(
-    #     pipeline.extract_descriptions(get_provided_examples()))]))
+    # pprint(list(enumerate(pipeline.extract_descriptions(cherry_picked_examples()))))
+    pprint(list([(i + 1, e) for (i, e) in enumerate(
+        pipeline.extract_descriptions(get_provided_examples()))]))
